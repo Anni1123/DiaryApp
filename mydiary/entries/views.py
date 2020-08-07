f5072252416ba52028f9b9a6637f977943acaaea
Anni1123 <kumarianni1213@gmail.com>
2020-08-07 14:10:35 +0530

    Completed

--- mydiary/entries/views.py
+++ mydiary/entries/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Entry
 # Create your views here.
 
+from .forms import EntryForm
 def index(request):
-    return render(request,'entries/index.html')
+    entries = Entry.objects.order_by('-date_posted')
+    context = {'entries': entries}
+    return render(request,'entries/index.html',context)
 
 def add(request):
-    return render(request,'entries/add.html')
+    if request.method == 'POST':
+        forms = EntryForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('home')
+    else:
+        forms=EntryForm()
+    context={'forms':forms}
+    return render(request,'entries/add.html',context)
